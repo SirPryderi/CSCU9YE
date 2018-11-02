116342e56ca81422064daca352e0c6190a521f24
Vittorio Iocolano <pryderi.mail@gmail.com>
2018-11-02 12:07:58 +0000

    Fix bug

--- utils/file_utils.py
+++ utils/file_utils.py
@@ -22,4 +22,4 @@ def load_file():
     dir_path = os.path.dirname(os.path.realpath(__file__))
     os.chdir(dir_path)  # Change the working directory so we can read the file
 
-    return read_file('colours.txt')  # Total number of colours and list of colours
+    return read_file('../colours.txt')  # Total number of colours and list of colours

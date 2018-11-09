0546df48b4ec260da80aabc69b98836901772504
Vittorio Iocolano <pryderi.mail@gmail.com>
2018-11-09 18:46:56 +0000

    Update benchmarks

--- benchmark.py
+++ benchmark.py
@@ -1,16 +1,19 @@
+import numpy as np
+
 from algorithms.greedyconstructive import greedy_constructive
 from algorithms.hillclimbing import hill_climb
 from algorithms.multi_hc import multi_hc
 from algorithms.spectrum import spectrum_sort_shake
-from utils.benchmark_utils import generate_subsets, benchmark_algorithm_scaling, benchmark_algorithm
-from utils.draw_utils import plot_progress, benchmark_plot_algorithm, benchmark_barchart_error, benchmark_boxplot
+from utils.benchmark_utils import generate_subsets, benchmark_algorithm, benchmark_algorithm_scaling
+from utils.draw_utils import plot_progress, plot_hc_results, benchmark_boxplot, benchmark_barchart_error, \
+    benchmark_plot_algorithm
 from utils.file_utils import load_file
 
 number_of_subsets = 20
-size_of_subsets = 200
+size_of_subsets = 500
 
 max_iterations = 100  # optimal value: 5000 - but extremely slow. Keep it to 100 for faster results
-hc_count = 20
+hc_count = 30
 
 algorithms = [
     ["Constructive", greedy_constructive, None, 'red'],
@@ -26,7 +29,15 @@ subsets = list(generate_subsets(colours, number_of_subsets, size_of_subsets))
 
 times = []
 for algorithm in algorithms:
-    times.append(list(benchmark_algorithm(subsets, algorithm[1], algorithm[2])))
+    algorithm_times = list(benchmark_algorithm(subsets, algorithm[1], algorithm[2]))
+    times.append(algorithm_times)
+    print("%s min/avg/max/std = %.2f/%.2f/%.2f/%.2f ms" % (
+        algorithm[0],
+        np.min(algorithm_times) * 1000,
+        np.average(algorithm_times) * 1000,
+        np.max(algorithm_times) * 1000,
+        np.std(algorithm_times) * 1000,
+    ))
 
 benchmark_boxplot(times, [row[0] for row in algorithms])
 
@@ -40,5 +51,7 @@ benchmark_plot_algorithm(algorithms, result, [2])
 progress = []
 
 hill_climb(subsets[0], max_iterations, progress)
+_, results = multi_hc(subsets[0], max_iterations, hc_count)
 
 plot_progress(progress, "Change of value during Hill Climbing (%d colours)" % size_of_subsets)
+plot_hc_results(results, 'Objective values (%d starts, %d colours)' % (len(results), size_of_subsets))
